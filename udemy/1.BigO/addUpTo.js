3fea24ad37057b847402aa42048cecf2f12a041f
Akinnawo3 <akinnawovictor@gmail.com>
2023-03-06 12:25:46 +0100

    compared the second method

--- udemy/1.BigO/addUpTo.js
+++ udemy/1.BigO/addUpTo.js
@@ -1,12 +1,18 @@
+//method 1 (using loop)
+// function addUpTo(n) {
+//   let total = 0;
+//   for (let i = 0; i < n; i++) {
+//     total += i;
+//   }
+//   return total;
+// }
+
+// mathod 2
 function addUpTo(n) {
-  let total = 0;
-  for (let i = 0; i < n; i++) {
-    total += i;
-  }
-  return total;
+  return n * n + 1 / 2;
 }
 
 const t1 = performance.now();
 console.log(addUpTo(10000000000));
 const t2 = performance.now();
-console.log(`the time elapsed is ${(t2-t1)/1000} seconds`)
+console.log(`the time elapsed is ${(t2 - t1) / 1000} seconds`);
