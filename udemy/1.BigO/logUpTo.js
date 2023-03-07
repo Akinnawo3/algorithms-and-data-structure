5eb6204c2df47f8acecc16463b164f0b43b13a88
Akinnawo3 <akinnawovictor@gmail.com>
2023-03-07 11:55:31 +0100

    side note

--- udemy/1.BigO/logUpTo.js
+++ udemy/1.BigO/logUpTo.js
@@ -7,3 +7,4 @@ function logAtMost10(n) {
 // Note that this has a complexity of O(1) beacause as from if n is > 10 , it only logs 10 times always.. so it is a constant value
 
 
+// In other words, the function logs a maximum of 10 always so theres no growth in the complexity relative to the value of n....... (it cant go past 10) . so the big(0) becomes contant at some point constant as 
