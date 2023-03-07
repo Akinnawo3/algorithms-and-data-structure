b1e19d3c27f28d5b02003de1dda6abc9ae222346
Akinnawo3 <akinnawovictor@gmail.com>
2023-03-07 11:36:24 +0100

    done

--- udemy/1.BigO/addUpTo.js
+++ udemy/1.BigO/addUpTo.js
@@ -1,7 +1,7 @@
 //method 1 (using loop)
 // function addUpTo(n) {
 //   let total = 0;
-//   for (let i = 0; i < n; i++) {
+//   for (let i = 0; i <= n; i++) {
 //     total += i;
 //   }
 //   return total;
@@ -9,10 +9,12 @@
 
 // mathod 2
 function addUpTo(n) {
-  return n * n + 1 / 2;
+  return n * (n + 1) / 2;
 }
 
 const t1 = performance.now();
-console.log(addUpTo(10000000000));
+console.log(addUpTo(30));
 const t2 = performance.now();
 console.log(`the time elapsed is ${(t2 - t1) / 1000} seconds`);
+
+
