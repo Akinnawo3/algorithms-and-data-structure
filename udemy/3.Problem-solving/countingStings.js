b5f926a78f907760cdc2b3d66225afdddd569735
Akinnawo3 <akinnawovictor@gmail.com>
2023-03-08 16:50:38 +0100

    problem solving

---
+++ udemy/3.Problem-solving/countingStings.js
@@ -0,0 +1,8 @@
+//  this function takes in a string and makes a count of all the individual alphabet or numbers in the string without considering the cases
+// it excludes special characters.
+// it returns the result in an object form.
+
+function count(str) {
+    let res ={}
+    
+}
