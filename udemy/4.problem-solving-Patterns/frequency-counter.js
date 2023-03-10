9143b124c58980cdb4fcedff3f0c8bd7dcbb8fae
Akinnawo3 <akinnawovictor@gmail.com>
2023-03-10 10:16:42 +0100

    check

--- udemy/4.problem-solving-Patterns/frequency-counter.js
+++ udemy/4.problem-solving-Patterns/frequency-counter.js
@@ -1,1 +1,7 @@
 // this pattern uses an object to collect a bunch of values and their frequencies
+
+// write a finction caled "same" which accepts two arrays. The value should return true
+// if every value in the array has its corresponding value squared in the second array .
+// the frequency must be the same
+
+function same(arr1, arr2) {}
