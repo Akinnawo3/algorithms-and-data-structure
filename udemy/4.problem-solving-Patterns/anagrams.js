75c1326adf348191037de02a63b35570a0c96e92
Akinnawo3 <akinnawovictor@gmail.com>
2023-03-10 12:10:11 +0100

    trying

--- udemy/4.problem-solving-Patterns/anagrams.js
+++ udemy/4.problem-solving-Patterns/anagrams.js
@@ -1,7 +1,7 @@
 // given two strings, write a function to determine if the second string is an anagram o f the first.
 // an anagram is a word fromed by rearranging the letters of another such as "cinema" and "iceman"
 
-
-function isAnagram(str1, str2){
-    
+function isAnagram(str1, str2) {
+  let counter1 = {};
+  let counter2 = {};
 }
