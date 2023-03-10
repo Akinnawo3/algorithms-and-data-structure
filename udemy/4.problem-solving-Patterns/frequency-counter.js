b86083742340d0590d9406cafa7668920dd78826
Akinnawo3 <akinnawovictor@gmail.com>
2023-03-10 10:28:33 +0100

    downloaded solution

--- udemy/4.problem-solving-Patterns/frequency-counter.js
+++ udemy/4.problem-solving-Patterns/frequency-counter.js
@@ -4,4 +4,26 @@
 // if every value in the array has its corresponding value squared in the second array .
 // the frequency must be the same
 
-function same(arr1, arr2) {}
+e.g;
+
+// same([1,2,3], [4,1,9]) returns true
+// same([1,2,3], [1,9]) returns false
+// same([1,2,1], [4,4,1]) returns false (must ebe the )
+
+//naive solution
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    let correctIndex = arr2.indexOf(arr1[i] ** 2);
+    if (correctIndex === -1) {
+      return false;
+    }
+    console.log(arr2);
+    arr2.splice(correctIndex, 1);
+  }
+  return true;
+}
+
+same([1, 2, 3, 2], [9, 1, 4, 4]);
