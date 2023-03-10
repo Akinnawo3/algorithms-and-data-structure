eb284334b4c798b985965a401e9978bc90fc2b66
Akinnawo3 <akinnawovictor@gmail.com>
2023-03-10 22:37:26 +0100

    done

--- udemy/4.problem-solving-Patterns/multiple-pointers.js
+++ udemy/4.problem-solving-Patterns/multiple-pointers.js
@@ -18,8 +18,14 @@ function sumZero(arr) {
 
 function sumZero(arr) {
   let left = 0;
-  let right= arr.length - 1;
-  while (left < right)
+  let right = arr.length - 1;
+  while (left < right) {
+    let sum = arr[left] + arr[right];
+    if (sum === 0) {
+      return [arr[left], arr[right]];
+      
+    }
+  }
 }
 
 sumZero([-4, -3, -2, -1, 0, 1, 2, 5]);
