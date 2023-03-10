3b13f3b6eca3d57dddd28ded1fe8d47f650889d2
Akinnawo3 <akinnawovictor@gmail.com>
2023-03-10 11:57:42 +0100

    got it

--- udemy/4.problem-solving-Patterns/frequency-counter.js
+++ udemy/4.problem-solving-Patterns/frequency-counter.js
@@ -11,19 +11,49 @@
 // same([1,2,1], [4,4,1]) returns false (must ebe the )
 
 //naive solution
+// function same(arr1, arr2) {
+//   if (arr1.length !== arr2.length) {
+//     return false;
+//   }
+//   for (let i = 0; i < arr1.length; i++) {
+//     let correctIndex = arr2.indexOf(arr1[i] ** 2);
+//     if (correctIndex === -1) {
+//       return false;
+//     }
+//     console.log(arr2);
+//     arr2.splice(correctIndex, 1);
+//   }
+//   return true;
+// }
+
+// console.log(same([1, 2, 3, 2], [9, 1, 4, 4]));
+
+//refactored solution
+
 function same(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
-  for (let i = 0; i < arr1.length; i++) {
-    let correctIndex = arr2.indexOf(arr1[i] ** 2);
-    if (correctIndex === -1) {
+  let frequencyCounter1 = {};
+  let frequencyCounter2 = {};
+  for (let val of arr1) {
+    frequencyCounter1[val] = ++frequencyCounter1[val] || 1;
+  }
+  for (let val of arr2) {
+    frequencyCounter2[val] = (frequencyCounter2[val] || 0) + 1;
+  }
+  console.log(frequencyCounter1);
+  console.log(frequencyCounter2);
+  for (let key in frequencyCounter1) {
+    if (!(key ** 2 in frequencyCounter2)) {
       return false;
     }
-    console.log(arr2);
-    arr2.splice(correctIndex, 1);
+    if (frequencyCounter2[key ** 2] !== frequencyCounter1[key]) {
+      return false;
+    }
   }
   return true;
 }
 
-console.log(same([1, 2, 3, 2], [9, 1, 4, 4]));
+console.log(same([1, 2, 3, 2, 5], [9, 1, 4, 4, 11]));
+// console.log(same([1, 2, 3, 2], [9, 1, 4, 4]));
