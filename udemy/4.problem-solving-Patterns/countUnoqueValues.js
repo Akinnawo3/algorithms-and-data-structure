921bfc591cfe66182e579dbf4b25acb1b37d59b3
Akinnawo3 <akinnawovictor@gmail.com>
2023-03-10 22:48:15 +0100

    done writing the question

---
+++ udemy/4.problem-solving-Patterns/countUnoqueValues.js
@@ -0,0 +1,3 @@
+// implement a function called "countUniqueValues",
+//  which accepts a sorted array and count the unique values in the array
+// there can be negative values in the array but it will always be sorted
