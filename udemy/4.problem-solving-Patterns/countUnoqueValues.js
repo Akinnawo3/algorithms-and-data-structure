5861009829610e843dcc8e4d4bde7d66b1be0c7c
Akinnawo3 <akinnawovictor@gmail.com>
2023-03-14 20:06:28 +0100

    more ooptimal solution

--- udemy/4.problem-solving-Patterns/countUnoqueValues.js
+++ udemy/4.problem-solving-Patterns/countUnoqueValues.js
@@ -3,17 +3,17 @@
 // there can be negative values in the array but it will always be sorted
 
 function countUniqueValues(arr) {
-  let j = 0;
-  for (let i in arr) {
-    if (arr[i] > arr[j]) {
-      arr.splice(j + 1, 0, arr[i]);
-      j += 2;
-      //   console.log(arr[i])
-      //   console.log(arr)
-      //   console.log(j)
-    }
-  }
-  return j;
+  //   let j = 0;
+  //   for (let i in arr) {
+  //     if (arr[i] > arr[j]) {
+  //       arr.splice(j + 1, 0, arr[i]);
+  //       j += 2;
+  //       //   console.log(arr[i])
+  //       //   console.log(arr)
+  //       //   console.log(j)
+  //     }
+  //   }
+  //   return j;
 
   //   const res = [];
 
@@ -23,6 +23,18 @@ function countUniqueValues(arr) {
   //     }
   //   }
   //   return res.length;
+
+  let counter1 = arr[0];
+  let counter2 = 1;
+
+  for (let i of arr) {
+    if (counter1 !== i) {
+      counter2++;
+      counter1 = i;
+    }
+  }
+  return counter2;
 }
 
 console.log(countUniqueValues([-2, -1, -1, 0, 1]));
+console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]));
