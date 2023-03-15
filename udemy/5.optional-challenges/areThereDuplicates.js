dc7586f2ffae1fe4171a254571ae1c2e84e5f59b
Akinnawo3 <akinnawovictor@gmail.com>
2023-03-15 11:39:55 +0100

    other options

--- udemy/5.optional-challenges/areThereDuplicates.js
+++ udemy/5.optional-challenges/areThereDuplicates.js
@@ -13,6 +13,43 @@ function areThereDuplicates(...args) {
   return false;
 }
 
+// areThereDuplicates Solution (Frequency Counter)
+// function areThereDuplicates() {
+//   let collection = {}
+//   for(let val in arguments){
+//     collection[arguments[val]] = (collection[arguments[val]] || 0) + 1
+//   }
+//   for(let key in collection){
+//     if(collection[key] > 1) return true
+//   }
+//   return false;
+// }
+// areThereDuplicates Solution (Multiple Pointers)
+// function areThereDuplicates(...args) {
+//   // Two pointers
+//   args.sort((a,b) => a > b);
+//   let start = 0;
+//   let next = 1;
+//   while(next < args.length){
+//     if(args[start] === args[next]){
+//         return true
+//     }
+//     start++
+//     next++
+//   }
+//   return false
+// }
+
+
+
+// areThereDuplicates One Liner Solution
+// function areThereDuplicates() {
+//   return new Set(arguments).size !== arguments.length;
+// }
+
+
+
+
 console.log(areThereDuplicates(1, 2, 3)); // false
 console.log(areThereDuplicates(1, 2, 2)); // true
 console.log(areThereDuplicates("a", "b", "c", "a")); // true
