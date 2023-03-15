7051985de40217640b4e3369e9f67fdc2cc4cb24
Akinnawo3 <akinnawovictor@gmail.com>
2023-03-15 08:07:58 +0100

    calc

--- udemy/4.problem-solving-Patterns/maxSubarraySum.js
+++ udemy/4.problem-solving-Patterns/maxSubarraySum.js
@@ -1,3 +1,8 @@
+// write a function called "maxSubarraySum" which accepts an array of integers 
+// and a number called "n" . the function should calculate the maximum
+// sum of n consecutive integer in the array.
+
+
 function maxSubarraySum(){
-    
+
 }
