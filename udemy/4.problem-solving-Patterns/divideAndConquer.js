e766af417bf45d97f03303506e657858b12502fe
Akinnawo3 <akinnawovictor@gmail.com>
2023-03-15 09:04:55 +0100

    done

---
+++ udemy/4.problem-solving-Patterns/divideAndConquer.js
@@ -0,0 +1,9 @@
+//wwrite a function that that takes in two parameters "arr" and "num"
+// the function should determine the index of the "num" in the array
+
+//using divide and conquer approach
+
+
+function determineTheIndex(arr, num){
+    
+}
