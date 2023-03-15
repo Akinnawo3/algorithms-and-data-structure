f20f1c70439a7812319ad073bfbe18a046f7c2b6
Akinnawo3 <akinnawovictor@gmail.com>
2023-03-15 08:40:13 +0100

    testing

--- udemy/4.problem-solving-Patterns/maxSubarraySum.js
+++ udemy/4.problem-solving-Patterns/maxSubarraySum.js
@@ -2,9 +2,20 @@
 // and a number called "n" . the function should calculate the maximum
 // sum of n consecutive integer in the array.
 
-function maxSubarraySum(arr, num) {}
+function maxSubarraySum(arr, num) {
+  if (arr.length < num) return null;
+
+  let maxsum = -Infinity;
+  let tempsum = -Infinity;
+
+  for (let i = 0; i < num; i++) {
+    maxsum += arr[i];
+    tempsum = maxsum;
+  }
+
+  return [maxsum, tempsum];
+}
 
 console.log(maxSubarraySum([2, 4, 6, 1, 9, 6, 4, 7, 8, 3, 2], 3));
-
 
 // expects 19
