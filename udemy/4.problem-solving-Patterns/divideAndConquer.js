eb03eab1e121c89db5a72c87e83b915aeb28529d
Akinnawo3 <akinnawovictor@gmail.com>
2023-03-15 09:35:52 +0100

    writing function

--- udemy/4.problem-solving-Patterns/divideAndConquer.js
+++ udemy/4.problem-solving-Patterns/divideAndConquer.js
@@ -7,9 +7,7 @@ function determineTheIndex(arr, num) {
   let min = 0;
   let max = arr.length - 1;
 
-  while (min<=max){
-    
+  while (min <= max) {
+    let middle = Math.floor((max - min) / 2);
   }
-
-
 }
