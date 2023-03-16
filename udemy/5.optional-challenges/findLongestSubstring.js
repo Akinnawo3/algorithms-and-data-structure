f650f01c6276854536606a9eb2c95cffbd248036
Akinnawo3 <akinnawovictor@gmail.com>
2023-03-16 23:09:48 +0100

    done

---
+++ udemy/5.optional-challenges/findLongestSubstring.js
@@ -0,0 +1,23 @@
+function findLongestSubstring(str) {
+  // add whatever parameters you deem necessary - good luck!
+  let accumulator = "";
+  let currentLongest = 0;
+  let i = 0,
+    j = 0;
+
+  if (str.length === 0) return 0;
+
+  while (i < str.length) {
+    if (!accumulator.includes(str[i])) {
+      accumulator += str[i];
+      i++;
+    } else {
+      currentLongest = Math.max(currentLongest, accumulator.length);
+      accumulator = "";
+      j++;
+      i = j;
+    }
+  }
+
+  return Math.max(currentLongest, accumulator.length);
+}
