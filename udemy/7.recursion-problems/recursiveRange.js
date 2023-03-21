a1dc49dc8193e17dedab8748f9d191ce088078d8
Akinnawo3 <akinnawovictor@gmail.com>
2023-03-21 21:42:27 +0100

    recursive range

---
+++ udemy/7.recursion-problems/recursiveRange.js
@@ -0,0 +1,9 @@
+// Write a function called recursiveRange which accepts a number 
+// and adds up all the numbers from 0 to the number passed to the function 
+
+
+
+function recursiveRange(num){
+    
+    return num === 0 ? 0 : num+ recursiveRange(num-1)
+}
