f54a28f159dbc93a05359ed7d72bd6e1b28aee99
Akinnawo3 <akinnawovictor@gmail.com>
2023-03-21 21:48:53 +0100

    issue

--- udemy/7.recursion-problems/fib.js
+++ udemy/7.recursion-problems/fib.js
@@ -3,3 +3,7 @@
 // Fibonacci sequence. Recall that the Fibonacci sequence is the sequence of 
 // whole numbers 1, 1, 2, 3, 5, 8, ... which starts with 1 and 1, and where every number thereafter 
 // is equal to the sum of the previous two numbers.
+
+function fib(){
+    // add whatever parameters you deem necessary - good luck!  
+  }
