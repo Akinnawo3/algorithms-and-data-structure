41370dc3b4814aece2cd17cec4bb202b0a1953d0
Akinnawo3 <akinnawovictor@gmail.com>
2023-04-09 00:11:14 +0100

    acommit

--- udemy/10.ssorting-algorithm/bubbleSort.js
+++ udemy/10.ssorting-algorithm/bubbleSort.js
@@ -1,6 +1,21 @@
-function bubbleSort(arr) {
-
-
+function swap(arr, smaller, larger) {
+  let temp = arr[smaller];
+  arr[smaller] = arr[larger];
+  arr[larger] = temp;
 }
 
-console.log(bubbleSort([4, 5, 2, 7, 2, 63, 8, 2, 0, 1, 9]));
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (j = 0; j < arr.length; j++) {
+      if (arr[j] < arr[j + 1]) {
+        swap(arr, j, j + 1);
+      }
+      console.log(arr);
+    }
+    console.log("ONE ROUND FINISHED !!!!");
+  }
+}
+
+// console.log(bubbleSort([4, 5, 2, 7, 2, 63, 8, 2, 0, 1, 9]));
+
+bubbleSort([4, 5, 2, 7, 2, 63, 8, 2, 0, 1, 9]);
