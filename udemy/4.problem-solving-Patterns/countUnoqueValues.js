3c14412532661ddd50e4287727877998e11e3a99
Akinnawo3 <akinnawovictor@gmail.com>
2023-11-28 13:21:42 +0100

    D

--- udemy/4.problem-solving-Patterns/countUnoqueValues.js
+++ udemy/4.problem-solving-Patterns/countUnoqueValues.js
@@ -3,38 +3,43 @@
 // there can be negative values in the array but it will always be sorted
 
 function countUniqueValues(arr) {
-  //   let j = 0;
-  //   for (let i in arr) {
-  //     if (arr[i] > arr[j]) {
-  //       arr.splice(j + 1, 0, arr[i]);
-  //       j += 2;
-  //       //   console.log(arr[i])
-  //       //   console.log(arr)
-  //       //   console.log(j)
-  //     }
-  //   }
-  //   return j;
+  // [-2, -1, -1, 0, 1]
 
-  //   const res = [];
+  
+  let j = 0;
+  for (let i in arr) {
+    if (arr[i] > arr[j]) {
+      arr.splice(j + 1, 0, arr[i]);
+      j += 2;
 
-  //   for (item of arr) {
-  //     if (!res.includes(item)) {
-  //       res.push(item);
-  //     }
-  //   }
-  //   return res.length;
-
-  let counter1 = arr[0];
-  let counter2 = 1;
-
-  for (let i of arr) {
-    if (counter1 !== i) {
-      counter2++;
-      counter1 = i;
+      console.log(arr);
+      //   console.log(arr[i])
+      //   console.log(arr)
+      //   console.log(j)
     }
   }
-  return counter2;
+  return j;
+
+  // const res = [];
+
+  // for (item of arr) {
+  //   if (!res.includes(item)) {
+  //     res.push(item);
+  //   }
+  // }
+  // return res.length;
+
+  // let counter1 = arr[0];
+  // let counter2 = 1;
+
+  // for (let i of arr) {
+  //   if (counter1 !== i) {
+  //     counter2++;
+  //     counter1 = i;
+  //   }
+  // }
+  // return counter2;
 }
 
 console.log(countUniqueValues([-2, -1, -1, 0, 1]));
-console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13]));
+// console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));
