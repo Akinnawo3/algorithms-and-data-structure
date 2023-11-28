7f309a3f5b5bfb7597a03114539e8cb4fdec1421
Akinnawo3 <akinnawovictor@gmail.com>
2023-11-28 14:18:56 +0100

    D

--- udemy/4.problem-solving-Patterns/countUnoqueValues.js
+++ udemy/4.problem-solving-Patterns/countUnoqueValues.js
@@ -3,22 +3,20 @@
 // there can be negative values in the array but it will always be sorted
 
 function countUniqueValues(arr) {
-  // [-2, -1, -1, 0, 1]
 
-  
-  let j = 0;
-  for (let i in arr) {
-    if (arr[i] > arr[j]) {
-      arr.splice(j + 1, 0, arr[i]);
-      j += 2;
-
-      console.log(arr);
-      //   console.log(arr[i])
-      //   console.log(arr)
-      //   console.log(j)
-    }
-  }
-  return j;
+  // let j = 0;
+  // for (let i in arr) {
+  //   console.log(arr[i])
+  //   if (arr[i] > arr[j]) {
+  //     arr.splice(j + 1, 0, arr[i]);
+  //     j += 2;
+  //     // console.log(arr);
+  //     //   console.log(arr[i])
+  //     //   console.log(arr)
+  //     //   console.log(j)
+  //   }
+  // }
+  // return j;
 
   // const res = [];
 
@@ -29,16 +27,19 @@ function countUniqueValues(arr) {
   // }
   // return res.length;
 
-  // let counter1 = arr[0];
-  // let counter2 = 1;
 
-  // for (let i of arr) {
-  //   if (counter1 !== i) {
-  //     counter2++;
-  //     counter1 = i;
-  //   }
-  // }
-  // return counter2;
+  // [-2, -1, -1, 0, 1]
+
+  let counter1 = arr[0];
+  let counter2 = 1;
+
+  for (let i of arr) {
+    if (counter1 !== i) {
+      counter2++;
+      counter1 = i;
+    }
+  }
+  return counter2;
 }
 
 console.log(countUniqueValues([-2, -1, -1, 0, 1]));
