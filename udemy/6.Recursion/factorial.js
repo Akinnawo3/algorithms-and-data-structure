1cc5d30107713491ca92ed7a8bdcf9187d21cfd7
Akinnawo3 <akinnawovictor@gmail.com>
2023-12-22 23:31:04 +0100

    fib

--- udemy/6.Recursion/factorial.js
+++ udemy/6.Recursion/factorial.js
@@ -4,18 +4,23 @@
 //  and all the integers below it; e.g., factorial four ( 4! ) is equal to
 //  24, because 4 * 3 * 2 * 1 equals 24.  factorial zero (0!) is always 1.
 
+// using while loop
+// function factorial(num) {
+//   let ans = 1;
+//   let newNum = num;
+
+//   while (newNum > 1) {
+//     ans = ans * newNum;
+//     newNum--;
+//   }
+
+//   return ans;
+// }
+
+// using recursion
 function factorial(num) {
-  let ans = 1;
-
-  let newNum;
-
-  while (num > 1) {
-    ans = ans * num;
-    num--;
-  }
-
-  // if (num === 1 || num === 0) return 1;
-  // return num * factorial(num - 1);
+    if (num === 1 || num === 0) return 1;
+    return num * factorial(num - 1);
 }
 
 console.log(factorial(5));
